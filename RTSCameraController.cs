2055a5b3e806d4aa3511e66ca9eadfd26c63229e
Antti Kangas <antti.kangas99@gmail.com>
2020-01-17 15:06:48 +0200

    Add keyboard movement

---
+++ RTSCameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTSCamera
+{
+    /// <summary>
+    /// RTS style camera controls.
+    /// </summary>
+    public class RTSCameraController : MonoBehaviour
+    {
+        /// <summary>
+        /// Move the camera instantly.
+        /// </summary>
+        /// <param name="dir">The point relative to the cameras position.</param>
+        public void Move(Vector2 dir)
+        {
+            transform.Translate(new Vector3(dir.x, 0f, dir.y), Space.World);
+        }
+    }
+}
---
+++ RTSCameraInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTSCamera
+{
+    /// <summary>
+    /// Optional input class for the controller.
+    /// </summary>
+    [RequireComponent(typeof(RTSCameraController))]
+    public class RTSCameraInput : MonoBehaviour
+    {
+        [Header("Types of movement")]
+        //Keyboard movement
+        public bool wasd = true;
+
+        //Speed of camera movement
+        public float speed = 5f;
+
+        new RTSCameraController camera;
+
+        private void Awake()
+        {
+            camera = GetComponent<RTSCameraController>();
+        }
+
+        void Update()
+        {
+            //Keyboard movement
+            if(wasd)
+            {
+                Vector2 dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+                camera.Move(dir);
+            }
+        }
+    }
+}
